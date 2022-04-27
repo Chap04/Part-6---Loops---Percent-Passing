212b346262a6e467e69e19fd6b345b57baf53801
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-04-27 10:37:12 -0400

    Add project files.

---
+++ Part 6 - Loops - Percent Passing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Part_6___Loops___Percent_Passing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            double passingCounter = 0;
+            double totalCounter = 0;
+            double gradeStore;
+            double totalGrades = 0;
+            double percentPassing;
+            double average;
+
+            //Grades Reading
+            Console.WriteLine("Enter grades to calcuate percentage passing, as well as average.\nType any non-numerical value to calculate.");
+            while (double.TryParse(Console.ReadLine(), out gradeStore))
+            {
+                if (gradeStore > 70)
+                {
+                    passingCounter++;
+                    totalCounter++;
+                    totalGrades = totalGrades + gradeStore;
+                }
+                else if (gradeStore < 71)
+                {
+                    totalCounter++;
+                    totalGrades = totalGrades + gradeStore;
+                }
+            }
+            average = totalGrades / totalCounter;
+            percentPassing = passingCounter / totalCounter * 100;
+            Console.WriteLine($"Thank you for the grades. The percentage of students passing is {Math.Round(percentPassing, 2)}%, or {passingCounter} out of {totalCounter}, with an average grade of {Math.Round(average, 2)}.");
+        }
+    }
+}
